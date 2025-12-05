dcf951c27f8cd6653c1d6cb619c3840957895230
Jaysen Naidoo <jaysen@gmail.com>
2025-12-05 21:15:39 +0200

    refactor: remove unused ConverterViewModel from MainWindowViewModel

--- src/WikiTool.Desktop/ViewModels/MainWindowViewModel.cs
+++ src/WikiTool.Desktop/ViewModels/MainWindowViewModel.cs
@@ -4,13 +4,11 @@ namespace WikiTool.Desktop.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public ConverterViewModel ConverterViewModel { get; }
     public WikiBrowserViewModel WikiBrowserViewModel { get; }
 
     public MainWindowViewModel()
     {
         var folderPickerService = new FolderPickerService();
-        ConverterViewModel = new ConverterViewModel(folderPickerService);
         WikiBrowserViewModel = new WikiBrowserViewModel(folderPickerService);
     }
 }
